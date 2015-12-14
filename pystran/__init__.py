a870cab948a6c6d89645d21091ab6d5f737f44a8
stvhoey <stvhoey.vanhoey@ugent.be>
2015-12-14 18:03:29 +0100

    started pystran as python package

---
+++ pystran/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Stijn Van Hoey
+
+pySTAN: python STRucture ANalyst
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+__version__ = "0.0.2"
+
+
+if __name__ == '__main__':
+    print 'pySTAN: python STRucture ANalyst (Van Hoey S. 2012)'
